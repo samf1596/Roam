dcfb44994b85bec3de12c0a5cf05153fd6528235
Sam Fox <sof5207@psu.edu>
2018-12-10 19:37:58 -0500

    first commit to private repo

---
+++ Roam/User Profile/ProfileHeaderCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileHeaderCollectionViewCell.swift
+//  Roam
+//
+//  Created by Samuel Fox on 12/1/18.
+//  Copyright © 2018 sof5207. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderCollectionViewCell: UICollectionViewCell {
+    
+}
